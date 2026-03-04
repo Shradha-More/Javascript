ee7f74893e5a7ce9f1f1fbeef2e329616838ef64
Shraddha More <spmore2907@gmail.com>
2026-03-04 09:59:51 +0000

    Added

---
+++ 07_projects/project2-BMICalculator/code.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form')
+// this usecase will give you empty
+//  const height = (document.querySelector('#height').value)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = (document.querySelector('#height').value)
+    const weight = (document.querySelector('#weight').value)
+    const results = document.querySelector('#results');
+
+if (height == '' || height < 0 || isNaN(height)){
+    results.innerHTML = `Please give a valid ${height}`;
+    } else if (height == '' || height < 0 || isNaN(height)){
+    results.innerHTML = `Please give a valid ${height}`;
+    } else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        // show the result
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+})
